a11d9d459dd534efc0616ce2e798bd68871b5f15
Tyko van Vliet <tykovv@gmail.com>
2021-05-16 19:34:16 -0400

    reached C linked at high VM range

--- src/start.s
+++ src/start.s
@@ -30,16 +30,13 @@ _bss_loop:
 	ldr x0, =_vector
 	msr VBAR_EL1, x0
 
-	mov x7, 0x47
-	mov x8, x0
-
 	// setup the mmu
 
 	// setup 4 KB granules in TTBR1 and TTBR0, and 16 MSB not checked
 	ldr x0, =(0b10 << 30) | (0b11 << 28) | (0b0101 << 24) | (16 << 16) | (0b11 << 12) | (0b0101 << 8) | 16
 	msr TCR_EL1, x0
 
-	// setup memory atribute 0 as normal memory
+	// setup memory attribute 0 as normal memory
 	mov x0, 0xFF
 	msr MAIR_EL1, x0
 
@@ -48,6 +45,7 @@ _bss_loop:
 	// map it into the first level 1 translation table
 	// this maps the first 512 GB of VM
 	ldr x0, =_tt_lv0
+	sub x0, x0, x4
 	// table entry, not block entry
 	mov x1, 0b11
 	// the address of the table
@@ -56,29 +54,19 @@ _bss_loop:
 	orr x1, x1, x2
 	// save this configuration in the table
 	str x1, [x0]
-	// setup both TTBRs to map into this
+	// finally save this setup in TTBR1
 	msr TTBR1_EL1, x0
-	msr TTBR0_EL1, x0
-
-	mov x0, 0x48
 	
 	// flat map the entire 1 GB of physical memory at 0xFFFF000000000000
 	// the first entry of this stage 1 table controls this memory
 	ldr x0, =_tt_lv1
+	sub x0, x0, x4
 	ldr x1, =0x0 // physical memory starts at 0
 	mov x2, 0x701 // should be good config for RW in EL1
 	orr x1, x1, x2
 	str x1, [x0]
-
 	dsb sy
 	isb
-
-
-
-	//mrs x7, TTBR0_EL1
-	//mrs x8, TTBR1_EL1
-	//mrs x9, TCR_EL1
-	//mov x10, x0
 
 	// enable the mmu
 	tlbi ALLE1
@@ -89,32 +77,22 @@ _bss_loop:
 	msr SCTLR_EL1, x0
 	dsb sy
 	isb
-	
-/*
-	adr x0, _paging_enabled
-	ldr x1, =__virtual_start
-	add x0, x0, x1
-	br x0
-_paging_enabled:
-	mov x0, 0x45
-	b .
-*/
 
 	// setup saved mode as EL1, using SP_EL1
 	mov x0, #0b0101
 	msr SPSR_EL2, x0
 
 	// set "return point" to just below after eret
-	adr x0, _el1_entry
+	// this also transfers control to using virtual addresses
+	ldr x0, =_el1_entry
 	msr ELR_EL2, x0
 	// move into EL1
 	eret
 _el1_entry:
+	// also operating in the high 48 bit VM range now, as MMU is enabled in EL1
 	// stack must be 16-bytes aligned
-	ldr x0, =0x20000000
+	ldr x0, =0xFFFF000000080000
 	mov sp, x0
-
-
 
 	bl main
 _infinite_loop:
